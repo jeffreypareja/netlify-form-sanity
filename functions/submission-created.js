88a3f23262a52a9c74f717166ff61d320cc14594
Jefrey Pareja <jmpareja15@halcyondigital@gmail.com>
2019-08-06 07:47:47 +0800

    testing using my own sanity

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,8 +1,7 @@
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
-  projectId: 'zv292tg5',
+  projectId: 'c2cua1gm',
   dataset: 'production',
-  token: process.env.SANITY_TOKEN
 })
 
 exports.handler = async function (event, context, callback) {
@@ -10,5 +9,5 @@ exports.handler = async function (event, context, callback) {
   const result = await client.create({ _type: 'submission.form', ...payload })
   callback(null, {
     statusCode: 200
-  })
+  }) 
 }
